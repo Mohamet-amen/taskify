99fc3abe1fedac3471d18e5e8a6506156386fefe
Mohamet-amen <mohametamiin77@gmail.com>
2024-11-04 21:32:54 +0300

    function added

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,3 +1,37 @@
+const { IncomingForm } = require('formidable');
+const { readTasksFromFile } = require("../utils/fileHandler")
+const { copyFileSync } = require('fs');
 exports.getTasks = (req, res)=>{
-    
+    const tasks = readTasksFromFile();
+    res.writeHead(200,  {'content-type':'application/json'})
+    res.end(JSON.stringify(tasks))
+}
+
+exports.createTask = (req, res)=>{
+    const form = new IncomingForm ();
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            res.writeHead(400, {'content-type':'application/json'});
+            res.end(JSON.stringify({
+            message:'Error Passing Form'
+        })) 
+        }
+        const tasks = readTasksFromFile()
+        const newTask = {
+            id: Date.now(),
+            title: fields.title[0],
+            description: fields?.description || '',
+            status: fields?.status || 'Pending',
+            image: fields.image ? `/uploads/${files.image.name}`: null ,
+        }
+
+        tasks.push(newTask)
+        writeTasksToFile(tasks)
+
+        if (files.image) {
+            copyFileSync(files.image.path, path.join(__dirname, '../uploads', files.image.name));
+            res.end(JSON.stringify(newTask))
+        }
+        
+})
 }
--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,8 +1,17 @@
+const { JSONParser } = require("formidable/parsers");
+
 const fs = required ('fs');
 const path= required('path');
 
 const filePath= './data/tasks.json';
 
-const writeTasksToFile = (tasks) => {
+exports.writeTasksToFile = (tasks) => {
     fs.writeFileSync(filePath, JSON.stringify(tasks));
+}
+exports.readTasksFromFile=()=>{
+    if (fs.existsSync(filePath)) {
+        writeTasksToFile ([])
+    }
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data);
 }
