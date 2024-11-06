ef97bad76bb78c3babd0b49c9c4ad1948f2c1efd
Mohamet-amen <mohametamiin77@gmail.com>
2024-11-06 20:07:36 +0300

    File hadnler Funtion Changed

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,31 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.resolve(__dirname, './data/tasks.json');
+const filePath = './data/tasks.json' ;
 
 exports.writeTasksToFile = (tasks) => {
-    const dir = path.dirname(filePath);
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
 
-    if (Array.isArray(tasks) && tasks.length === 0) {
+    if (Array.isArray(tasks) && tasks.length < 1) {
         fs.writeFileSync(filePath, '[]'); 
-    } else {
+    } 
         fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
-    }
+    
 };
 
 exports.readTasksFromFile = () => {
     if (!fs.existsSync(filePath)) {
         this.writeTasksToFile([]);
     }
-
-    try {
-        const data = fs.readFileSync(filePath);
-        return JSON.parse(data);
-    } catch (error) {
-        exports.writeTasksToFile([]);
-        return [];
-    }
-};
+        exports.writeTasksToFile(filePath);
+        return JSON.parse(data) ;
+    };
